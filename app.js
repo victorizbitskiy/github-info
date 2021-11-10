90d674563533b313df23932f2c61ce6d836f9d60
victorizbitskiy <victor.izbitskiy@yandex.ru>
2021-11-10 20:27:19 +0300

    added avatar

--- app.js
+++ app.js
@@ -21,23 +21,28 @@ function sendRequest(method, url) {
   })
 }
 
+
+
 sendRequest('GET', requestURL)
   .then(data => {
     const body = document.getElementById('body')
     console.log(data);
-    addStatsToHtml('Статистика', data)
+    addStatsToHtml('Статистика GitHub', data)
   })
 //   .cath(err => console.log(err))
 
 function addStatsToHtml(title, content) {
 
   document.body.innerHTML = `
-    <h1>${title}</h1>
-    <div class="stats">id: ${content.id}</div>
-    <div class="stats">name:${content.name}</div>
-    <div class="stats">followers:${content.followers}</div>
-    <div class="stats">following:${content.following}</div>
-
-  `
-
+    <h1 class="title">${title}</h1>
+    <img class="avatar" src="${content.avatar_url}"/>
+    <div class="stats">
+      <div class="attribute"> login: <span class="attributeValue">${content.login}</span></div>
+      <div class="attribute"> name: <span class="attributeValue">${content.name}</span></div>
+      <div class="attribute"> bio: <span class="attributeValue">${content.bio}</span></div>
+      <div class="attribute"> blog: <span class="attributeValue">${content.blog}</span></div>
+      <div class="attribute"> followers: <span class="attributeValue">${content.followers}</span></div>
+      <div class="attribute"> following: <span class="attributeValue">${content.following}</span></div>
+    </div>
+    `
 }
